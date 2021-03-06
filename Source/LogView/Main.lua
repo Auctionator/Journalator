afc56f54630c9ab4ae3face71dea86429f5ef3bd
plusmouse <plusmouse@protonmail.com>
2021-03-06 15:28:44 +0000

    Remove dead code

--- Source/LogView/Main.lua
+++ Source/LogView/Main.lua
@@ -1,9 +1,5 @@
 JournalatorLogViewMixin = CreateFromMixins(AuctionatorEscapeToCloseMixin)
 
-function JournalatorLogViewMixin:OnLoad()
-  self.HistoricalPriceListing:Init(self.HistoricalPriceProvider)
-end
-
 function JournalatorLogViewMixin:OnCloseClicked()
   self:Hide()
 end
