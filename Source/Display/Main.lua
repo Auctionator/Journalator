6ac79277eccc71941681c9c0ce5bf4b094321779
plusmouse <plusmouse@protonmail.com>
2021-03-09 10:23:03 +0000

    Add CSV export option

--- Source/Display/Main.lua
+++ Source/Display/Main.lua
@@ -3,6 +3,8 @@ JournalatorDisplayMixin = {}
 function JournalatorDisplayMixin:OnLoad()
   PanelTemplates_SetNumTabs(self, #self.Tabs)
   table.insert(UISpecialFrames, self:GetName())
+
+  self:SetupExportCSVDialog()
 end
 
 function JournalatorDisplayMixin:OnShow()
@@ -10,6 +12,8 @@ function JournalatorDisplayMixin:OnShow()
 end
 
 function JournalatorDisplayMixin:SetDisplayMode(displayMode)
+  self.exportCSVDialog:Hide()
+
   for index, tab in ipairs(self.Tabs) do
     if tab.displayMode == displayMode then
       PanelTemplates_SetTab(self, index)
@@ -20,5 +24,21 @@ function JournalatorDisplayMixin:SetDisplayMode(displayMode)
 
   for _, view in ipairs(self.Views) do
     view:SetShown(view.displayMode == displayMode)
+  end
+end
+
+function JournalatorDisplayMixin:SetupExportCSVDialog()
+  self.exportCSVDialog = CreateFrame("Frame", "JournalatorExportCSVTextFrame", self, "AuctionatorExportTextFrame")
+  self.exportCSVDialog:SetPoint("CENTER")
+end
+
+function JournalatorDisplayMixin:ExportCSVClicked()
+  for _, view in ipairs(self.Views) do
+    if view:IsShown() then
+      view.DataProvider:GetCSV(function(result)
+        self.exportCSVDialog:SetExportString(result)
+        self.exportCSVDialog:Show()
+      end)
+    end
   end
 end
