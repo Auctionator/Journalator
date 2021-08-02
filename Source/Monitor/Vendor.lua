e174a5aa4103043e3ea7961ce0e8e0eaa083b580
plusmouse <plusmouse@protonmail.com>
2021-08-02 11:19:10 +0100

    Support purchases done with addon automation

--- Source/Monitor/Vendor.lua
+++ Source/Monitor/Vendor.lua
@@ -2,81 +2,67 @@ JournalatorVendorMonitorMixin = {}
 
 local EQUIPMENT_SLOT_CAP = 19
 
-local function GetSalesIDFromLocation(location)
+local function GetGUIDFromLocation(location)
   if location:IsValid() then
     return C_Item.GetItemGUID(location) .. " " .. C_Item.GetStackCount(location)
   end
 end
 
-local function GetSalesIDFromBagAndSlot(bag, slot)
+local function GetGUIDFromBagAndSlot(bag, slot)
   local location = ItemLocation:CreateFromBagAndSlot(bag, slot)
-  return GetSalesIDFromLocation(location)
+  return GetGUIDFromLocation(location)
 end
 
-local function GetSalesIDFromEquipmentSlot(slot)
+local function GetGUIDFromEquipmentSlot(slot)
   local location = ItemLocation:CreateFromEquipmentSlot(slot)
-  return GetSalesIDFromLocation(location)
+  return GetGUIDFromLocation(location)
 end
 
-local function IsSalesIDInPossession(salesID)
+local function IsGUIDInPossession(guid)
+  -- Check if an item in a bag has disappeared/been sold.
   for bag = 0, 4 do
     -- Start the slots at 0 in include the container's item
     for slot = 0, GetContainerNumSlots(bag) do
-      if GetSalesIDFromBagAndSlot(bag, slot) == salesID then
+      if GetGUIDFromBagAndSlot(bag, slot) == guid then
         return true
       end
     end
   end
 
+  -- Check if an equipped item has disappeared/been sold
   for equipmentSlot = 1, EQUIPMENT_SLOT_CAP do
-    if GetSalesIDFromEquipmentSlot(equipmentSlot) == salesID then
+    if GetGUIDFromEquipmentSlot(equipmentSlot) == guid then
       return true
     end
   end
+
   return false
 end
 
-local function GetAllSalesIDs()
+local function GetGUIDStackSizes()
   local result = {}
+
   for bag = 0, 4 do
     -- Start the slots at 0 in include the container's item
     for slot = 0, GetContainerNumSlots(bag) do
-      local salesID = GetSalesIDFromBagAndSlot(bag, slot)
-      if salesID ~= nil then
-        table.insert(result, salesID)
+      local guid = GetGUIDFromBagAndSlot(bag, slot)
+      if guid ~= nil then
+        local _, count, _, _, _, _, itemLink = GetContainerItemInfo(bag, slot)
+        result[guid] = {count = count, itemLink = itemLink}
       end
     end
   end
 
-  for equipmentSlot = 1, EQUIPMENT_SLOT_CAP do
-    local salesID = GetSalesIDFromEquipmentSlot(equipmentSlot)
-    if salesID ~= nil then
-      table.insert(result, salesID)
-    end
-  end
+  --We don't check equipped items as you can't buy directly to an equipment slot
 
   return result
 end
 
-local function IsAnyNewSalesIDs(oldSalesIDs)
-  for bag = 0, 4 do
-    for slot = 1, GetContainerNumSlots(bag) do
-      local salesID = GetSalesIDFromBagAndSlot(bag, slot)
-      if salesID ~= nil and tIndexOf(oldSalesIDs, salesID) == nil then
-        return true
-      end
-    end
-  end
-
-  return false
-end
-
 function JournalatorVendorMonitorMixin:OnLoad()
-  -- Used to detect the successful sale of an item
-  self.expectedToUpdate = {}
+  self:ResetExpected()
 
   FrameUtil.RegisterFrameForEvents(self, {
-    "MERCHANT_SHOW", "MERCHANT_CLOSED", "MERCHANT_UPDATE", "BAG_UPDATE"
+    "MERCHANT_SHOW", "MERCHANT_CLOSED", "MERCHANT_UPDATE"
   })
   self:RegisterRightClickToSellHandlers()
   self:RegisterDragToSellHandlers()
@@ -88,6 +74,12 @@ end
 
 function JournalatorVendorMonitorMixin:OnUpdate()
   self:CheckCursorItemsForDragging()
+end
+
+  -- Used to detect the successful sale of an item
+function JournalatorVendorMonitorMixin:ResetExpected()
+  self.expectedToSell = {}
+  self.expectedToPurchase = {}
 end
 
 function JournalatorVendorMonitorMixin:RegisterRightClickToSellHandlers()
@@ -102,10 +94,10 @@ function JournalatorVendorMonitorMixin:RegisterRightClickToSellHandlers()
     if item:IsItemEmpty() then
       return
     end
-    local salesID = GetSalesIDFromBagAndSlot(bag, slot)
+    local guid = GetGUIDFromBagAndSlot(bag, slot)
     item:ContinueOnItemLoad(function()
       local vendorPrice = select(Auctionator.Constants.ITEM_INFO.SELL_PRICE, GetItemInfo(itemLink))
-      self.expectedToUpdate[salesID] = {
+      self.expectedToSell[guid] = {
         vendorType = "sell",
         itemName = Journalator.Utilities.GetNameFromLink(itemLink),
         count = itemCount,
@@ -127,24 +119,24 @@ function JournalatorVendorMonitorMixin:UpdateCursorItem()
 
   local itemLocation = C_Cursor.GetCursorItem()
   if itemLocation then
-    local salesID = GetSalesIDFromLocation(itemLocation)
+    local guid = GetGUIDFromLocation(itemLocation)
     local itemLink = C_Item.GetItemLink(itemLocation)
     local itemCount = C_Item.GetStackCount(itemLocation)
 
-    self.expectedToUpdate[salesID] = nil
+    self.expectedToSell[guid] = nil
 
     local item = Item:CreateFromItemLocation(itemLocation)
     item:ContinueOnItemLoad(function()
       -- If this item has been queued to be sold, it should no longer be on the
       -- cursor and in the queue, we just reset the queue so it works to check
       -- it.
-      if self.expectedToUpdate[salesID] ~= nil then
+      if self.expectedToSell[guid] ~= nil then
         return
       end
 
       local vendorPrice = select(Auctionator.Constants.ITEM_INFO.SELL_PRICE, GetItemInfo(itemLink))
       self.lastCursorItem = {
-        salesID = salesID,
+        guid = guid,
         item = {
           vendorType = "sell",
           itemName = Journalator.Utilities.GetNameFromLink(itemLink),
@@ -166,7 +158,7 @@ function JournalatorVendorMonitorMixin:RegisterDragToSellHandlers()
     end
 
     if self.lastCursorItem ~= nil and C_Cursor.GetCursorItem() == nil then
-      self.expectedToUpdate[self.lastCursorItem.salesID] = self.lastCursorItem.item
+      self.expectedToSell[self.lastCursorItem.guid] = self.lastCursorItem.item
     end
     self.lastCursorItem = nil
   end)
@@ -202,7 +194,7 @@ function JournalatorVendorMonitorMixin:RegisterBuybackHandlers()
     local name, _, price, quantity = GetBuybackItemInfo(index)
     local link = GetBuybackItemLink(index)
 
-    self.expectedToUpdate["buy"] = {
+    self.expectedToSell["buy"] = {
       vendorType = "buyback",
       itemName = name,
       count = quantity,
@@ -211,7 +203,6 @@ function JournalatorVendorMonitorMixin:RegisterBuybackHandlers()
       time = time(),
       source = Journalator.State.Source,
     }
-    self.lastScannedSalesIDs = GetAllSalesIDs()
   end)
 end
 
@@ -227,7 +218,7 @@ function JournalatorVendorMonitorMixin:RegisterPurchaseHandlers()
     local link = GetMerchantItemLink(index)
 
     if price ~= 0 and numInStock ~= 0 then
-      self.expectedToUpdate["buy"] = {
+      table.insert(self.expectedToPurchase,{
         vendorType = "purchase",
         itemName = name,
         count = quantity,
@@ -235,8 +226,16 @@ function JournalatorVendorMonitorMixin:RegisterPurchaseHandlers()
         itemLink = link,
         time = time(),
         source = Journalator.State.Source,
-      }
-      self.lastScannedSalesIDs = GetAllSalesIDs()
+      })
+      -- Sort in descending order. Used to determine which purchases have gone
+      -- through.
+      table.sort(self.expectedToPurchase, function(a, b)
+        if a.itemLink == b.itemLink then
+          return b.count < a.count
+        else
+          return a.itemLink < b.itemLink
+        end
+      end)
     end
   end)
 end
@@ -244,31 +243,55 @@ end
 function JournalatorVendorMonitorMixin:OnEvent(eventName, ...)
   if eventName == "MERCHANT_SHOW" then
     self:SetScript("OnUpdate", self.OnUpdate)
+    self:RegisterEvent("BAG_UPDATE")
+
+    self:ResetExpected()
+    self.oldStackSizes = GetGUIDStackSizes()
     self.merchantShown = true
 
   elseif eventName == "MERCHANT_CLOSED" then
     self:SetScript("OnUpdate", nil)
-    self.expectedToUpdate = {}
+    self:UnregisterEvent("BAG_UPDATE")
+
+    self:ResetExpected()
     self.merchantShown = false
 
-  elseif eventName == "MERCHANT_CLOSED" then
-
-  elseif eventName == "MERCHANT_UPDATE" or eventName == "BAG_UPDATE" then
-    for salesID, item in pairs(self.expectedToUpdate) do
+  elseif eventName == "MERCHANT_UPDATE" then
+    for guid, item in pairs(self.expectedToSell) do
       -- Check if an item sold to the vendor has disappeared from the player's
       -- bag
-      if item.vendorType == "sell" and not IsSalesIDInPossession(salesID) then
+      if not IsGUIDInPossession(guid) then
         table.insert(Journalator.State.Logs.Vendoring, item)
-        self.expectedToUpdate[salesID] = nil
-
-      -- Check if any new item has appeared in the player's bag (which is enough
-      -- to check if a buyback item has been bought back)
-      elseif (item.vendorType == "purchase" or item.vendorType == "buyback")
-          and IsAnyNewSalesIDs(self.lastScannedSalesIDs) then
-        table.insert(Journalator.State.Logs.Vendoring, item)
-        self.lastScannedSalesIDs = GetAllSalesIDs()
-        self.expectedToUpdate[salesID] = nil
+        self.expectedToSell[guid] = nil
       end
     end
+
+  elseif eventName == "BAG_UPDATE" then
+    local newStackSizes = GetGUIDStackSizes()
+    local newExpected = {}
+    for _, item in ipairs(self.expectedToPurchase) do
+
+      local foundMatch = false
+
+      for guid, details in pairs(newStackSizes) do
+        if details.itemLink == item.itemLink then
+          if self.oldStackSizes[guid] == nil then
+            foundMatch = true
+            break
+          elseif newStackSizes[guid].count - self.oldStackSizes[guid].count >= item.count then
+            foundMatch = true
+            self.oldStackSizes[guid].count = self.oldStackSizes[guid].count + item.count
+            break
+          end
+        end
+      end
+
+      if foundMatch then
+        table.insert(Journalator.State.Logs.Vendoring, item)
+      else
+        table.insert(newExpected, item)
+      end
+    end
+    self.oldStackSizes = newStackSizes
   end
 end
