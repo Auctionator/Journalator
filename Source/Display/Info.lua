638627fbd1d810677cbc9c00b9b8b87c5e4c088e
plusmouse <plusmouse@protonmail.com>
2021-08-03 17:35:57 +0100

    Add "Open Addon Options" button to the Journalator tab

--- Source/Display/Info.lua
+++ Source/Display/Info.lua
@@ -2,3 +2,8 @@ JournalatorInfoDisplayMixin = {}
 
 function JournalatorInfoDisplayMixin:OnShow()
 end
+
+function JournalatorInfoDisplayMixin:OpenOptions()
+  InterfaceOptionsFrame:Show()
+  InterfaceOptionsFrame_OpenToCategory(JOURNALATOR_L_JOURNALATOR)
+end
