8ccedaab5ecda0093246fcecc5d29c915ca54529
plusmouse <plusmouse@protonmail.com>
2022-02-07 20:00:05 +0000

    Remove now unused code for refresh button

--- Source/Display/DataTab.lua
+++ Source/Display/DataTab.lua
@@ -5,11 +5,6 @@ function JournalatorDataTabDisplayMixin:OnLoad()
   self.ResultsListing:Init(self.DataProvider)
 end
 
-function JournalatorDataTabDisplayMixin:RefreshButtonClicked()
-  self.DataProvider:Refresh()
-  self:GetParent().Filters:UpdateRealms()
-end
-
 function JournalatorDataTabDisplayMixin:OnUpdate()
   self.DataProvider:SetFilters(self:GetParent().Filters:GetFilters())
 
