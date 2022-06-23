eb9908c57a292805f30ba359c4f085ac7e8f9fec
plusmouse <plusmouse@protonmail.com>
2022-06-23 12:02:38 +0100

    [Fixes #21] Processing a lot of the same item quickly causes them to not all be recorded

--- Source/Monitor/Mail.lua
+++ Source/Monitor/Mail.lua
@@ -20,7 +20,7 @@ function JournalatorMailMonitorMixin:OnLoad()
 end
 
 local function GetMailKey(mail)
-  return mail.header[4] .. tostring(mail.header[7])
+  return mail.header[4] .. " " .. tostring(mail.header[7]) .. " " .. tostring(mail.invoice[11] or 0)
 end
 
 local function CacheMail(index)
@@ -39,27 +39,44 @@ local function GetFirstItem(mailIndex)
   end
 end
 
-local function RecordAllMail(itemLog)
+local function RecordAllMail(itemLog, counts)
   local cache = {}
+  local newCounts = {}
   for i = 1, GetInboxNumItems() do
     local mail = CacheMail(i)
     local key = GetMailKey(mail)
 
     cache[key] = mail
     itemLog[key] = GetFirstItem(i) or itemLog[key]
+
+    if newCounts[key] == nil then
+      newCounts[key] = 0
+    end
+
+    if mail.invoice[11] ~= nil then
+      newCounts[key] = newCounts[key] + mail.invoice[11]
+    else
+      newCounts[key] = newCounts[key] + 1
+    end
+
+    if counts[key] == nil or newCounts[key] > counts[key] then
+      counts[key] = newCounts[key]
+    end
   end
-  return cache, itemLog
+  return cache, itemLog, counts
 end
 
 --local invoiceType, itemName, playerName, bid, _, deposit, consignment, _, _, _, count, _ = GetInboxInvoiceInfo(index)
-local function SaveInvoice(mail, itemLink)
+--count: Number of items seen across multiple similar mails for this item, used
+--to avoid losing items when processing a lot of them.
+local function SaveInvoice(mail, itemLink, count)
   Journalator.AddToLogs({ Invoices = {
     {
       invoiceType = mail.invoice[1],
       itemName = mail.invoice[2],
       playerName = mail.invoice[3],
       value = mail.invoice[4],
-      count = mail.invoice[11],
+      count = count,
       deposit = mail.invoice[6],
       consignment = mail.invoice[7],
       time = time(),
@@ -69,7 +86,9 @@ local function SaveInvoice(mail, itemLink)
   }})
 end
 
-local function SaveFailed(failedType, itemInfo, itemLink)
+--multiplier: Number of similar mails for this item and quantity, used to avoid
+--losing items when processing a lot of them.
+local function SaveFailed(failedType, itemInfo, itemLink, multiplier)
   local itemName, quantityText = string.match(itemInfo, "(.*) %((%d+)%)")
 
   local quantity = 1
@@ -83,7 +102,7 @@ local function SaveFailed(failedType, itemInfo, itemLink)
     {
     failedType = failedType,
     itemName = itemName,
-    count = quantity,
+    count = quantity * multiplier,
     time = time(),
     source = Journalator.State.Source,
     itemLink = itemLink,
@@ -98,17 +117,18 @@ function JournalatorMailMonitorMixin:OnEvent(eventName, ...)
   if eventName == "MAIL_INBOX_UPDATE" then
     if not next(self.previousCache) then
       self.itemLog = {}
-      self.previousCache = RecordAllMail(self.itemLog)
+      self.counts = {}
+      self.previousCache = RecordAllMail(self.itemLog, self.counts)
     end
-    local newCache = RecordAllMail(self.itemLog)
+    local newCache = RecordAllMail(self.itemLog, self.counts)
     for key, mail in pairs(self.previousCache) do
       if newCache[key] == nil and mail.header[4] ~= RETRIEVING_DATA then
         if mail.invoice[1] ~= nil then
-          SaveInvoice(mail, self.itemLog[key])
+          SaveInvoice(mail, self.itemLog[key], self.counts[key])
         elseif string.match(mail.header[4], expiredText) then
-          SaveFailed("expired", string.match(mail.header[4], expiredText), self.itemLog[key])
+          SaveFailed("expired", string.match(mail.header[4], expiredText), self.itemLog[key], self.counts[key])
         elseif string.match(mail.header[4], cancelledText) then
-          SaveFailed("cancelled", string.match(mail.header[4], cancelledText), self.itemLog[key])
+          SaveFailed("cancelled", string.match(mail.header[4], cancelledText), self.itemLog[key], self.counts[key])
         end
       end
     end
