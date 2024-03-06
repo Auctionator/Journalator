0b1d0b777aadf29d0be4b8536e2e8998a5fe1e14
plusmouse <plusmouse@protonmail.com>
2024-03-06 22:11:49 +0000

    Use LibBattlePetTooltipLine to show some details on the RHS of the pet tooltip

--- Journalator_Statistics/Tooltips/Hooks.lua
+++ Journalator_Statistics/Tooltips/Hooks.lua
@@ -43,7 +43,7 @@ hooksecurefunc(Auctionator.Tooltip, "ShowTipWithPricingDBKey",
     tooltipFrame:Show()
 end)
 
-local PET_TOOLTIP_SPACING = " "
+local LibBattlePetTooltipLine = LibStub("LibBattlePetTooltipLine-1-0")
 
 hooksecurefunc(Auctionator.Tooltip, "AddPetTip",
   function(speciesID)
@@ -60,21 +60,21 @@ hooksecurefunc(Auctionator.Tooltip, "AddPetTip",
     local salesRate, failedCount, lastSold, lastBought, boughtStats, soldStats = Journalator.Tooltips.GetSalesInfo(itemName)
 
     if salesRate ~= nil then
-      BattlePetTooltip:AddLine(JOURNALATOR_L_SALE_RATE .. PET_TOOLTIP_SPACING .. WHITE_FONT_COLOR:WrapTextInColorCode(salesRate))
+      LibBattlePetTooltipLine:AddDoubleLine(BattlePetTooltip, JOURNALATOR_L_SALE_RATE, WHITE_FONT_COLOR:WrapTextInColorCode(salesRate))
     end
     if failedCount ~= nil then
-      BattlePetTooltip:AddLine(JOURNALATOR_L_FAILURES .. PET_TOOLTIP_SPACING .. WHITE_FONT_COLOR:WrapTextInColorCode(failedCount))
+      LibBattlePetTooltipLine:AddDoubleLine(BattlePetTooltip, JOURNALATOR_L_FAILURES, WHITE_FONT_COLOR:WrapTextInColorCode(failedCount))
     end
     if lastSold ~= nil then
-      BattlePetTooltip:AddLine(JOURNALATOR_L_LAST_SOLD .. PET_TOOLTIP_SPACING .. WHITE_FONT_COLOR:WrapTextInColorCode(Auctionator.Utilities.CreatePaddedMoneyString(lastSold)))
+      LibBattlePetTooltipLine:AddDoubleLine(BattlePetTooltip, JOURNALATOR_L_LAST_SOLD, WHITE_FONT_COLOR:WrapTextInColorCode(Auctionator.Utilities.CreatePaddedMoneyString(lastSold)))
     end
     if lastBought ~= nil then
-      BattlePetTooltip:AddLine(JOURNALATOR_L_LAST_BOUGHT .. PET_TOOLTIP_SPACING .. WHITE_FONT_COLOR:WrapTextInColorCode(Auctionator.Utilities.CreatePaddedMoneyString(lastBought)))
+      LibBattlePetTooltipLine:AddDoubleLine(BattlePetTooltip, JOURNALATOR_L_LAST_BOUGHT, WHITE_FONT_COLOR:WrapTextInColorCode(Auctionator.Utilities.CreatePaddedMoneyString(lastBought)))
     end
     if boughtStats ~= nil then
-      BattlePetTooltip:AddLine(JOURNALATOR_L_BOUGHT_MIN_MAX_MEAN:format(ColorMoney(GetMoneyString(boughtStats.min, true)), ColorMoney(GetMoneyString(boughtStats.max)), ColorMoney(GetMoneyString(boughtStats.mean))))
+      LibBattlePetTooltipLine:AddLine(BattlePetTooltip, JOURNALATOR_L_BOUGHT_MIN_MAX_MEAN:format(ColorMoney(GetMoneyString(boughtStats.min, true)), ColorMoney(GetMoneyString(boughtStats.max)), ColorMoney(GetMoneyString(boughtStats.mean))))
     end
     if soldStats ~= nil then
-     BattlePetTooltip:AddLine(JOURNALATOR_L_SOLD_MIN_MAX_MEAN:format(ColorMoney(GetMoneyString(soldStats.min, true)), ColorMoney(GetMoneyString(soldStats.max)), ColorMoney(GetMoneyString(soldStats.mean))))
+     LibBattlePetTooltipLine:AddLine(JOURNALATOR_L_SOLD_MIN_MAX_MEAN:format(ColorMoney(GetMoneyString(soldStats.min, true)), ColorMoney(GetMoneyString(soldStats.max)), ColorMoney(GetMoneyString(soldStats.mean))))
     end
 end)
